67fe77983da0596f0a944bc940e54259859d3035
jperamak <jussi.peramai@gmail.com>
2014-02-27 21:35:18 +0200

    kaviot kopsuu oikein

--- RUISunity/Assets/OpenNI/Scripts/audioScripts/AudioFootstepsWhenWalking.cs
+++ RUISunity/Assets/OpenNI/Scripts/audioScripts/AudioFootstepsWhenWalking.cs
@@ -5,21 +5,23 @@ public class AudioFootstepsWhenWalking : MonoBehaviour {
 	
 	
 	private AudioSource audio;
+	private RUISCharacterController charControl;
 	
 	// Use this for initialization
 	void Start () {
-		
+
+        charControl = gameObject.GetComponent<RUISCharacterController>();
 		audio = gameObject.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if (Input.GetButtonDown ("Horizontal") || Input.GetButtonDown ("Vertical") && !audio.isPlaying) 
+
+        if (charControl.grounded && !audio.isPlaying && ( Input.GetButton("Horizontal") || Input.GetButton("Vertical") ) ) 
 		{
 			audio.Play ();
-		} 
-		else if (!Input.GetButton ("Horizontal") && !Input.GetButton ("Vertical") && audio.isPlaying) 
+		}
+        else if (!charControl.grounded && audio.isPlaying || !Input.GetButton("Horizontal") && !Input.GetButton("Vertical") && audio.isPlaying ) 
 		{
 			audio.Stop ();
 		}
