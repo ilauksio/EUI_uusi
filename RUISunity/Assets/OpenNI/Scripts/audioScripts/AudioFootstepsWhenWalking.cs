3583309833146c458d22b32bf0073df9827027a8
ilauksio <iisa.lauksio@aalto.fi>
2014-02-27 17:11:44 +0200

    Initial commit

---
+++ RUISunity/Assets/OpenNI/Scripts/audioScripts/AudioFootstepsWhenWalking.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioFootstepsWhenWalking : MonoBehaviour {
+	
+	
+	private AudioSource audio;
+	
+	// Use this for initialization
+	void Start () {
+		
+		audio = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetButtonDown ("Horizontal") || Input.GetButtonDown ("Vertical") && !audio.isPlaying) 
+		{
+			audio.Play ();
+		} 
+		else if (!Input.GetButton ("Horizontal") && !Input.GetButton ("Vertical") && audio.isPlaying) 
+		{
+			audio.Stop ();
+		}
+		
+		
+	}
+}
---
+++ RUISunity/Assets/RUIS/Examples/Shared Files/Prefabs/AudioFallingCrate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioFallingCrate : MonoBehaviour {
+
+
+	private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+	
+		audio = gameObject.GetComponent<AudioSource>();
+	
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(audio != null)
+		{
+			audio.Play();
+		}
+	}
+	// Update is called once per frame
+	//void Update () {
+	//
+	//	audio.Play ();
+	//
+	//}
+}
